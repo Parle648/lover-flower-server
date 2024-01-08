3f5cdf9f151fe0e7b377a35948cf0a258ec1410b
Olexandr Morozov <106087433+Parle648@users.noreply.github.com>
2024-01-08 18:01:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const port = process.env.PORT || 3001;
-
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
-
 require('dotenv').config();
 const sequelize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
 const router = require('./routes/index');
 
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api', router);
+// app.use('/api', router);
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Everything working OK'})
